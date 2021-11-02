a8f3dd100b02934a44b7f562bb1e025a21be2d50
DafterWatch <marcosingefa@gmail.com>
2021-11-02 18:35:50 -0400

    confirmarcitas full

--- rutas.js
+++ rutas.js
@@ -94,6 +94,52 @@ router.get('/getDoctores/:id',(req, res)=>{
         }
     });
 });
+//get areas
+router.get('/getAreas',(req, res)=>{
+    let sql = 'select * from area'
+    conexion.query(sql, (err, rows, fields)=>{
+        if(err) throw err;
+        else{
+            res.json(rows);
+        }
+    });
+});
+//get 1 area
+router.get('/getAreas/:id',(req, res)=>{
+    const {id} = req.params
+    let sql = 'select * from area where id = ?'
+    conexion.query(sql,[id], (err, rows, fields)=>{
+        if(err) throw err;
+        else{
+            res.json(rows[0]);
+        }
+    });
+});
+//get 1 area por nombre
+router.get('/getAreasName/:areaName',(req, res)=>{
+    const {areaName} = req.params
+    let sql = `select d.id, d.doctorName, d.doctorDetails, d.doctorImg 
+    from doctores d inner join area a on d.doctorDetails = a.areaName
+    where a.areaName = ?`
+    conexion.query(sql,[areaName], (err, rows, fields)=>{
+        if(err) throw err;
+        else{
+            res.json(rows);
+        }
+    });
+});
+//agregar citas
+router.post('/addCita',(req, res)=>{
+    const {fecha, doctorId, pacienteId, estado} = req.body
+    let sql = `insert into citasMedicas(fecha, doctorId, pacienteId, estado)
+                values('${fecha}',${doctorId},${pacienteId},${estado})`
+    conexion.query(sql, (err, rows, fields)=>{
+        if(err) throw err
+        else {
+            res.json({status: 'usuario agregado'});
+        }
+    })
+});
 //--------------------
 
 module.exports = router;
